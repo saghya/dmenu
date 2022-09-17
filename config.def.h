01b68c4e708ebfe9e0db07f6d708418a95aa29e2
Saghya <saghyattila77@gmail.com>
2022-09-17 08:03:15 +0200

    tweak

--- config.def.h
+++ config.def.h
@@ -5,14 +5,14 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int min_width = 300;                 /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrains Mono:size=13"
+    "JetBrains Mono:size=13"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
+    /*     fg         bg       */
     [SchemeNorm] = { "#eff0eb", "#282a36" },
-	[SchemeSel]  = { "#282a36", "#69a2ff" },
-	[SchemeOut]  = { "#000000", "#00ffff" },
+    [SchemeSel]  = { "#282a36", "#69a2ff" },
+    [SchemeOut]  = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 10;
