f8ffef984afd3e91028b87824796f1526bef73f0
Saghya <saghyattila77@gmail.com>
2022-11-06 19:00:40 +0100

    retab

--- dmenu.c
+++ dmenu.c
@@ -337,19 +337,19 @@ movewordedge(int dir)
 static void
 keypress(XKeyEvent *ev)
 {
-	char buf[64];
+    char buf[64];
     int len;
-	KeySym ksym = NoSymbol;
+    KeySym ksym = NoSymbol;
     Status status;
 
     len = XmbLookupString(xic, ev, buf, sizeof buf, &ksym, &status);
     switch (status) {
     default: /* XLookupNone, XBufferOverflow */
         return;
-	case XLookupChars: /* composed string from input method */
+    case XLookupChars: /* composed string from input method */
         goto insert;
     case XLookupKeySym:
-	case XLookupBoth: /* a KeySym and a string are returned: use keysym */
+    case XLookupBoth: /* a KeySym and a string are returned: use keysym */
         break;
     }
 
@@ -713,23 +713,23 @@ static void
 readstdin(void)
 {
     char *line = NULL;
-	size_t i, junk, itemsiz = 0;
+    size_t i, junk, itemsiz = 0;
     ssize_t len;
 
     /* read each line from stdin and add it to the item list */
-	for (i = 0; (len = getline(&line, &junk, stdin)) != -1; i++) {
-		if (i + 1 >= itemsiz) {
-			itemsiz += 256;
-			if (!(items = realloc(items, itemsiz * sizeof(*items))))
-				die("cannot realloc %zu bytes:", itemsiz * sizeof(*items));
-		}
+    for (i = 0; (len = getline(&line, &junk, stdin)) != -1; i++) {
+        if (i + 1 >= itemsiz) {
+            itemsiz += 256;
+            if (!(items = realloc(items, itemsiz * sizeof(*items))))
+                die("cannot realloc %zu bytes:", itemsiz * sizeof(*items));
+        }
         if (line[len - 1] == '\n')
             line[len - 1] = '\0';
         items[i].text = line;
         items[i].out = 0;
-		line = NULL; /* next call of getline() allocates a new line */
+        line = NULL; /* next call of getline() allocates a new line */
     }
-	free(line);
+    free(line);
     if (items)
         items[i].text = NULL;
     lines = MIN(lines, i);
